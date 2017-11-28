2beb812d3da31e08d5986d22f8ae33da58615225
Gangemstyle850 <carterman123456@outlook.com>
2017-11-28 13:02:23 -0500

    Signed-off-by: Gangemstyle850 <carterman123456@outlook.com>

--- Cycle Reporter 2.0/MainActivity.cs
+++ Cycle Reporter 2.0/MainActivity.cs
@@ -19,8 +19,9 @@ namespace Cycle_Reporter_2._0
             
             base.OnCreate(savedInstanceState);
 
-            String serverIp = "192.168.43.133`";
-            String serverUrl = "http://192.168.43.133";
+            String serverIp = "www.cyclereporter.net";
+            String serverUrl = "http://www.cyclereporter.net/";
+            String apiUrl = "http://www.cyclereporter.net/api/mobile.php?";
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -29,7 +30,7 @@ namespace Cycle_Reporter_2._0
             viewReports.Click += delegate
             {
                 Intent i = new Intent(Intent.ActionView);
-                i.SetData(Android.Net.Uri.Parse(serverUrl));
+                i.SetData(Android.Net.Uri.Parse(serverUrl + "/view"));
                 StartActivity(i);
             };
         }
