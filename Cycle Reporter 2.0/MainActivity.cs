1cfaa60a9f796e7fd744ea35a4e01fc33dd2d739
Gangemstyle850 <carterman123456@outlook.com>
2017-11-28 17:31:25 -0500

    Additions & Updates

--- Cycle Reporter 2.0/MainActivity.cs
+++ Cycle Reporter 2.0/MainActivity.cs
@@ -5,9 +5,10 @@ using Android.Runtime;
 using System.Runtime.Remoting.Contexts;
 using System;
 using Android.Content;
-
-
-
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Json;
 
 namespace Cycle_Reporter_2._0
 {
@@ -16,28 +17,55 @@ namespace Cycle_Reporter_2._0
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            
             base.OnCreate(savedInstanceState);
 
             String serverIp = "www.cyclereporter.net";
             String serverUrl = "http://" + serverIp;
-            String apiUrl = serverUrl + "/api/mobile.php?";
+            String apiUrl = serverUrl + "/api/mobile.php";
 
             // Set View To Main Layout
             SetContentView(Resource.Layout.Main);
 
             //Take User To Report View Page, On Button Click
             Button viewReports = FindViewById<Button>(Resource.Id.viewReportsButton);
-            viewReports.Click += async (sender, e) => {
+            viewReports.Click += async delegate {
                 Intent i = new Intent(Intent.ActionView);
                 i.SetData(Android.Net.Uri.Parse(serverUrl + "/view"));
                 StartActivity(i);
             };
 
+            //Submit Report On Button Click
             Button submitButton = FindViewById<Button>(Resource.Id.submitButton);
-            submitButton.Click += async (sender, e) => {
-                //Submition Code Here
+            TextView reportTextview = FindViewById<TextView>(Resource.Id.reportText);
+            submitButton.Click += async delegate{
+                string reportText = reportTextview.Text;
+                if (reportText == "") {
+                    string apiUrlFinal = apiUrl + "?report=" + reportText;
+                    JsonValue json = await SubmitToServer(apiUrlFinal);
+                };
             };
+        }
+        private async Task<JsonValue> SubmitToServer(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
         }
     }
 }
--- Cycle Reporter 2.0/Resources/Resource.Designer.cs
+++ Cycle Reporter 2.0/Resources/Resource.Designer.cs
@@ -41,33 +41,17 @@ namespace Cycle_Reporter_2
 			}
 		}
 		
-		public partial class Drawable
-		{
-			
-			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
-			
-			static Drawable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Drawable()
-			{
-			}
-		}
-		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int reportText = 2131034114;
+			// aapt resource value: 0x7f040002
+			public const int reportText = 2130968578;
 			
-			// aapt resource value: 0x7f050000
-			public const int submitButton = 2131034112;
+			// aapt resource value: 0x7f040000
+			public const int submitButton = 2130968576;
 			
-			// aapt resource value: 0x7f050001
-			public const int viewReportsButton = 2131034113;
+			// aapt resource value: 0x7f040001
+			public const int viewReportsButton = 2130968577;
 			
 			static Id()
 			{
@@ -82,8 +66,8 @@ namespace Cycle_Reporter_2
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f020000
+			public const int Main = 2130837504;
 			
 			static Layout()
 			{
@@ -98,8 +82,8 @@ namespace Cycle_Reporter_2
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int app_name = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int app_name = 2130903040;
 			
 			static String()
 			{
